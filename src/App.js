b8b8efc88a2ff58b5d378003ca777a5cbcd69312
Adedayo Okubanjo <dayookubanjo@Adedayos-MacBook-Pro.local>
2022-10-11 13:08:06 -0400

    Before JSON server integration

--- src/App.js
+++ src/App.js
@@ -62,21 +62,3 @@ function App() {
 }
 
 export default App;
-
-// [
-//   {
-//       id: 1,
-//       checked: false,
-//       item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-//   },
-//   {
-//       id: 2,
-//       checked: false,
-//       item: "Item 2"
-//   },
-//   {
-//       id: 3,
-//       checked: false,
-//       item: "Item 3"
-//   }
-// ]
